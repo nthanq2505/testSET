abddbc940e76f4cdaf53f5e3b5c0161605d5c6ec
phongth803 <thp080203@mail.com>
2024-11-15 21:36:37 +0700

    done 3

--- todocoderpad/src/App.tsx
+++ todocoderpad/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { v4 as uuid } from "uuid";
 import styled from "@emotion/styled";
 import { AddInput } from "./components/AddInput";
@@ -14,32 +14,15 @@ const Wrapper = styled.div({
 });
 
 /**
-* This is the initial todo state.
-* Instead of loading this data on every reload,
-* we should save the todo state to local storage,
-* and restore on page load. This will give us
-* persistent storage.
-*/
-const initialData: Todo[] = [
-  {
-    id: uuid(),
-    label: "Buy groceries",
-    checked: false,
-  },
-  {
-    id: uuid(),
-    label: "Reboot computer",
-    checked: false,
-  },
-  {
-    id: uuid(),
-    label: "Ace CoderPad interview",
-    checked: true,
-  },
-];
+ * This is the initial todo state.
+ * Instead of loading this data on every reload,
+ * we should save the todo state to local storage,
+ * and restore on page load. This will give us
+ * persistent storage.
+ */
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>(initialData);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodo = useCallback((label: string) => {
     setTodos((prev) => [
@@ -52,16 +35,33 @@ function App() {
     ]);
   }, []);
 
-  const handleChange = useCallback((checked: boolean) => {
-    // handle the check/uncheck logic
+  const handleChange = useCallback((id: string, checked: boolean) => {
+    setTodos((prev) =>
+      prev.map((todo) => (todo.id === id ? { ...todo, checked } : todo))
+    );
   }, []);
+
+  useEffect(() => {
+    const todos = localStorage.getItem("todos");
+    if (todos) {
+      setTodos(JSON.parse(todos));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const sortedTodos = useMemo(() => {
+    return todos.sort((a, b) => (a.checked ? 1 : -1));
+  }, [todos]);
 
   return (
     <Wrapper>
       <Header>Todo List</Header>
       <AddInput onAdd={addTodo} />
       <TodoList>
-        {todos.map((todo) => (
+        {sortedTodos.map((todo) => (
           <TodoItem {...todo} onChange={handleChange} />
         ))}
       </TodoList>
--- todocoderpad/src/components/TodoItem.tsx
+++ todocoderpad/src/components/TodoItem.tsx
@@ -35,7 +35,7 @@ export interface TodoItemProps {
   id: string;
   label: string;
   checked?: boolean;
-  onChange?: (checked: boolean) => void;
+  onChange?: (id: string, checked: boolean) => void;
 }
 
 export const TodoItem: FC<TodoItemProps> = ({
@@ -50,7 +50,7 @@ export const TodoItem: FC<TodoItemProps> = ({
         type="checkbox"
         id={id}
         checked={checked}
-        onChange={(e) => onChange(e.target.checked)}
+        onChange={(e) => onChange(id, e.target.checked)}
       />
       <Label checked={checked}>{label}</Label>
     </Wrapper>
