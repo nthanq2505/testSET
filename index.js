c45761ef3b4ecb4174134a74ea537dca91835525
nthanq2505 <20127627@student.hcmus.edu.vn>
2024-11-22 18:27:36 +0700

    Fixed for VH-chan

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Bài 1a
-function sum1ToN(n) {
-    if (n < 0 || n > 1000) {
+function sum1ToN(number) {
+    if (number < 0 || number > 1000) {
         console.log('Invalid input');
         return;
     }
@@ -16,10 +16,10 @@ function sum1ToN(n) {
 // sum1ToN(10);
 
 // Bài 1b
-function sumTheDivisorsOfn(n) {
-    let sum = n;
-    for (let i = 1; i <= n / 2; i++) {
-        if (n % i == 0) {
+function sumTheDivisorsOfn(number) {
+    let sum = number;
+    for (let i = 1; i <= number / 2; i++) {
+        if (number % i == 0) {
             sum += i;
         }
     }
@@ -33,11 +33,11 @@ console.log(sumTheDivisorsOfn(16))
 
 
 // Bài 1c
-function sumTheDigitsOfn(n) {
+function sumTheDigitsOfn(number) {
     let sum = 0;
-    while (n > 0) {
-        sum += n % 10;
-        n = Math.floor(n / 10);
+    while (number > 0) {
+        sum += number % 10;
+        number = Math.floor(number / 10);
     }
     console.log(sum);
 }
@@ -46,14 +46,14 @@ sumTheDigitsOfn(123);
 sumTheDigitsOfn(456);
 
 // Bài 1d, 2a
-function LargestDigitOfn(n) {
+function LargestDigitOfn(number) {
     let max = 0;
-    while (n > 0) {
-        let currentNumber = n % 10;
+    while (number > 0) {
+        let currentNumber = number % 10;
         if (currentNumber > max) {
             max = currentNumber;
         }
-        n = Math.floor(n / 10);
+        number = Math.floor(number / 10);
     }
     console.log(max);
 }
@@ -61,67 +61,55 @@ LargestDigitOfn(123123123);
 LargestDigitOfn(4564564569);
 
 // Bài 2b
-function numToArray(n) {
-    let arr = []
-    while (n > 0) {
-        arr.push(n % 10);
-        n = Math.floor(n / 10);
+function numToArray(number) {
+    let array = []
+    while (number > 0) {
+        array.push(number % 10);
+        number = Math.floor(number / 10);
     }
-    return arr;
+    return array;
 }
 
 // Bài 5
-function sortArray(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] < arr[j]) {
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+function sortArray(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] < array[j]) {
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
             }
         }
     }
-    return arr;
+    return array;
 }
 
-function secondLargestDigitOfn(n) {
-    let arr = numToArray(n);
-    let sortedArr = sortArray(arr);
-    return sortedArr[1];
+function findSecondLargestDigitOfn(number) {
+    let array = numToArray(number);
+    let sortedArray = sortArray(array);
+    return sortedArray[1];
 }
 
 // Bài 2c
-function SmallestDigitOfn(n) {
-    let arr = numToArray(n);
-    let sortArray = sortArray(arr);
-    return sortArray[sortArray.length - 1];
+function findSmallestDigitOfn(number) {
+    let array = numToArray(number);
+    let sortedArray = sortArray(array);
+    return sortedArray[sortedArray.length - 1];
 }
 
 // Bài 3a
-// function isPrime(n) {
-//     if (n < 2) {
-//         return false;
-//     }
-//     for (let i = 2; i <= Math.sqrt(n); i++) {
-//         if (n % i == 0) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-function isPrime(n) {
-    if (n < 2) {
+function isPrime(number) {
+    if (number < 2) {
         return false;
     }
-    if (n === 2) {
+    if (number === 2) {
         return true;
     }
-    if (n % 2 === 0) {
+    if (number % 2 === 0) {
         return false;
     }
-    for (let i = 3; i <= Math.sqrt(n); i += 2) {
-        if (n % i === 0) {
+    for (let i = 3; i <= Math.sqrt(number); i += 2) {
+        if (number % i === 0) {
             return false;
         }
     }
@@ -129,12 +117,12 @@ function isPrime(n) {
 }
 
 // Bài 2d
-function getAllPrimeDigitOfn(n) {
-    let arr = numToArray(n);
+function getAllPrimeDigitOfn(number) {
+    let array = numToArray(number);
     let result = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (isPrime(arr[i])) {
-            result.push(arr[i]);
+    for (let i = 0; i < array.length; i++) {
+        if (isPrime(array[i])) {
+            result.push(array[i]);
         }
     }
     return result;
@@ -143,56 +131,42 @@ function getAllPrimeDigitOfn(n) {
 console.log(getAllPrimeDigitOfn(123456789).join(''));
 
 // Bài 3b
-function isPerfectNumber(n) {
-    return sumTheDivisorsOfn(n) == 2 * n;
+function isPerfectNumber(number) {
+    return sumTheDivisorsOfn(number) == 2 * number;
 }
 
 // Bài 3c
-// function isSquareNumber(n) {
-//     if (n < 4) {
-//         return true;
-//     } else if (n === 4) {
-//         return false;
-//     }
-//     for (let i = 3; i <= n / 2; i++) {
-//         if (i * i == n) {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
-
-function isSquareNumber(n) {
-    return Math.sqrt(n) % 1 === 0;
+function isSquareNumber(number) {
+    return Math.sqrt(number) % 1 === 0;
 }
 
 // Bài 4a
-function reverseString(str) {
+function reverseString(string) {
     let result = '';
-    for (let i = 0; i < str.length; i++) {
-        result = str[i] + result;
+    for (let i = 0; i < string.length; i++) {
+        result = string[i] + result;
     }
     return result;
 }
 
 // Bài 4b. delete all " " in string
-function deleteSpace(str) {
+function deleteSpace(string) {
     let result = '';
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] !== ' ') {
-            result += str[i];
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] !== ' ') {
+            result += string[i];
         }
     }
     return result;
 }
 
 //
-function isEqualsString(str1, str2) {
-    if (str1.length !== str2.length) {
+function isEqualsString(string1, string2) {
+    if (string1.length !== string2.length) {
         return false
     }
-    for (let i = 0; i < str1.length; i++) {
-        if (str1[i] !== str2[i]) {
+    for (let i = 0; i < string1.length; i++) {
+        if (string1[i] !== string2[i]) {
             return false;
         }
     }
@@ -200,26 +174,26 @@ function isEqualsString(str1, str2) {
 }
 
 // Bài 4c.
-function isPalindrome(str) {
-    return isEqualsString(str, reverseString(str));
+function isPalindrome(string) {
+    return isEqualsString(string, reverseString(string));
 }
 
-function getSubString(str, start, end) {
+function getSubString(string, start, end) {
     let result = '';
     for (let i = start; i < end; i++) {
-        result += str[i];
+        result += string[i];
     }
     return result;
 }
 
 // Bài 4d. Find the longest palindrome in a string
-function longestPalindrome(str) {
+function longestPalindrome(string) {
     let result = '';
-    for (let i = 0; i < str.length; i++) {
-        for (let j = i; j < str.length; j++) {
-            let subStr = getSubString(str, i, j + 1);
-            if (isPalindrome(subStr) && subStr.length > result.length) {
-                result = subStr;
+    for (let i = 0; i < string.length; i++) {
+        for (let j = i; j < string.length; j++) {
+            let subString = getSubString(string, i, j + 1);
+            if (isPalindrome(subString) && subString.length > result.length) {
+                result = subString;
             }
         }
     }
@@ -229,9 +203,9 @@ function longestPalindrome(str) {
 // Bài 5. dòng 74
 
 // Bài 6a.
-function factorial(n) {
-    let result = 1;
-    for (let i = 1; i <= n; i++) {
+function factorial(number) {
+    let result = 1; 
+    for (let i = 1; i <= number; i++) {
         result *= i;
     }
     return result;
@@ -240,17 +214,17 @@ function factorial(n) {
 // Bài 6b. Giống bài 1c
 
 // Bài 7. input n, output fibonacci(n)
-function fibonacci(n) {
-    if (n == 0) {
+function fibonacci(number) {
+    if (number == 0) {
         return 0;
     }
-    if (n == 1) {
+    if (number == 1) {
         return 1;
     }
     let a = 0;
     let b = 1;
     let c;
-    for (let i = 2; i <= n; i++) {
+    for (let i = 2; i <= number; i++) {
         c = a + b;
         a = b;
         b = c;
@@ -258,9 +232,9 @@ function fibonacci(n) {
     return c;
 }
 
-function fibonacciArray(n) {
+function fibonacciArray(number) {
     let arr = [];
-    for (let i = 0; i <= n; i++) {
+    for (let i = 0; i <= number; i++) {
         arr.push(fibonacci(i));
     }
     return arr;
@@ -270,8 +244,8 @@ console.log("Fibonacci: ");
 console.log(fibonacciArray(10));
 
 // Bài 8a. "ueoai"
-function isVowel(c) {
-    switch (c) {
+function isVowel(character) {
+    switch (character) {
         case 'u':
         case 'e':
         case 'o':
@@ -282,10 +256,10 @@ function isVowel(c) {
     return false;
 }
 
-function countVowel(str) {
+function countVowel(string) {
     let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (isVowel(str[i])) {
+    for (let i = 0; i < string.length; i++) {
+        if (isVowel(string[i])) {
             count++;
         }
     }
@@ -293,10 +267,10 @@ function countVowel(str) {
 }
 
 // Bài 8b. count exist
-function countExist(str, c) {
+function countExist(string, character) {
     let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === c) {
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === character) {
             count++;
         }
     }
@@ -304,10 +278,10 @@ function countExist(str, c) {
 }
 
 // Bài 8c. Đếm số ký tự xuất hiện nhiều nhất
-function countMostExist(str) {
+function countMostExist(string) {
     let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        let currentCount = countExist(str, str[i]);
+    for (let i = 0; i < string.length; i++) {
+        let currentCount = countExist(string, string[i]);
         if (currentCount > count) {
             count = currentCount;
         }
@@ -316,17 +290,17 @@ function countMostExist(str) {
 }
 
 // Bài 9a. Tìm dãy con tăng/giảm dài nhất
-function longestIncreaseSubArray(arr) {
+function longestIncreaseSubArray(array) {
     let result = [];
-    let temp = [arr[0]];
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > arr[i - 1]) {
-            temp.push(arr[i]);
+    let temp = [array[0]];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > array[i - 1]) {
+            temp.push(array[i]);
         } else {
             if (temp.length > result.length) {
                 result = temp;
             }
-            temp = [arr[i]];
+            temp = [array[i]];
         }
     }
     if (temp.length > result.length) {
@@ -336,40 +310,40 @@ function longestIncreaseSubArray(arr) {
 }
 
 // Bài 9b. Sắp xếp ký tự theo thứ tự tăng/giảm và in ra.
-function sortString(str, isIncrease) {
-    for (let i = 0; i < str.length - 1; i++) {
-        for (let j = i + 1; j < str.length; j++) {
-            if (isIncrease && str[i] > str[j] || !isIncrease && str[i] < str[j]) {
-                let temp = str[i];
-                str[i] = str[j];
-                str[j] = temp;
+function sortString(string, isIncrease) {
+    for (let i = 0; i < string.length - 1; i++) {
+        for (let j = i + 1; j < string.length; j++) {
+            if (isIncrease && string[i] > string[j] || !isIncrease && string[i] < string[j]) {
+                let temp = string[i];
+                string[i] = string[j];
+                string[j] = temp;
             }
         }
     }
 }
 
 // Bài 10a. Tìm ước chung lớn nhất
-function gcd(a, b) {
-    while (b != 0) {
-        let temp = a % b;
-        a = b;
-        b = temp;
+function gcd(number1, number2) {
+    while (number2 != 0) {
+        let temp = number1 % number2;
+        number1 = number2;
+        number2 = temp;
     }
-    return a;
+    return number1;
 }
 
 // Bài 10b. Tìm bội chung nhỏ nhất
-function lcm(a, b) {
-    return a * b / gcd(a, b);
+function lcm(number1, number2) {
+    return number1 * number2 / gcd(number1, number2);
 }
 
 // Bài 11a. Tìm vị trí số lớn nhất trong mảng
-function findMaxIndex(arr) {
-    let max = arr[0];
+function findMaxIndex(array) {
+    let max = array[0];
     let index = 0;
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
             index = i;
         }
     }
@@ -377,12 +351,12 @@ function findMaxIndex(arr) {
 }
 
 // Bài 11b. Tìm vị trí số nhỏ nhất trong mảng
-function findMinIndex(arr) {
-    let min = arr[0];
+function findMinIndex(array) {
+    let min = array[0];
     let index = 0;
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < min) {
-            min = arr[i];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
             index = i;
         }
     }
@@ -390,21 +364,21 @@ function findMinIndex(arr) {
 }
 
 // Bài 11c. Tính tổng mảng
-function sumArray(arr) {
+function sumOfArray(array) {
     let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
     }
     return sum;
 }
 
 // Bài 12a. Tìm số lớn nhất trong mảng 2 chiều
-function findMaxIn2DArray(arr) {
-    let max = arr[0][0];
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            if (arr[i][j] > max) {
-                max = arr[i][j];
+function findMaxIn2DArray(array) {
+    let max = array[0][0];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (array[i][j] > max) {
+                max = array[i][j];
             }
         }
     }
@@ -412,12 +386,12 @@ function findMaxIn2DArray(arr) {
 }
 
 // Bài 12b. Tìm số nhỏ nhất trong mảng 2 chiều
-function findMinIn2DArray(arr) {
-    let min = arr[0][0];
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            if (arr[i][j] < min) {
-                min = arr[i][j];
+function findMinIn2DArray(array) {
+    let min = array[0][0];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (array[i][j] < min) {
+                min = array[i][j];
             }
         }
     }
@@ -425,29 +399,29 @@ function findMinIn2DArray(arr) {
 }
 
 // Bài 12c. Tính tổng mảng 2 chiều trên hàng hoặc cột k
-function sumRow(arr, k) {
+function sumOfRow(array, k) {
     let sum = 0;
-    for (let i = 0; i < arr[k].length; i++) {
-        sum += arr[k][i];
+    for (let i = 0; i < array[k].length; i++) {
+        sum += array[k][i];
     }
     return sum;
 }
 
-function sumColumn(arr, k) {
+function sumOfColumn(array, k) {
     let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i][k];
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i][k];
     }
     return sum;
 }
 
 // Bài 12d. Tổng các số nguyên tố trong mảng 2 chiều
-function sumPrimeIn2DArray(arr) {
+function sumOfPrimeIn2DArray(array) {
     let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            if (isPrime(arr[i][j])) {
-                sum += arr[i][j];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (isPrime(array[i][j])) {
+                sum += array[i][j];
             }
         }
     }
